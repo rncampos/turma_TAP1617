12dfa4160a1c82b7f59aca866cbba70049d9af09
rncampos <ricardo.campos@ipt.pt>
2017-03-22 16:27:21 +0000

    Upload do Project - WebConsole

---
+++ WebConsole/WebConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyExtensionMethods;
+
+namespace WebConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string URL = "https://pt.wikipedia.org/wiki/C_Sharp";
+            string html = Web.GetHTMLContentFromURL(URL);
+            string textFromURL = Web.GetTextContentFromURL(URL);
+        }
+    }
+}
